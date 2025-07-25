4b0a8e44e2f477ff5d590ddb2c1f1ab0107dc024
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-07-25 14:14:34 +0100

    Update index.ts

--- routes/templates/index.ts
+++ routes/templates/index.ts
@@ -15,3 +15,5 @@ router.get("/:id", authenticate, getTemplate);
 router.post("/", authenticate, createTemplate);
 router.put("/:id", authenticate, updateTemplate);
 router.delete("/:id", authenticate, deleteTemplate);
+
+export default router
